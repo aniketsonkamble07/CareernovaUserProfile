e5b2006aab085bda9ed4d1501cee7dea3a04cccd
aniketsonkamble07 <aniketsonkamble07@gmail.com>
2026-02-28 13:45:04 +0530

    Add full user microservice code

---
+++ src/main/java/com/careernova/userservices/CareernovaApplication.java
@@ -0,0 +1,13 @@
+package com.careernova.userservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareernovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareernovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careernova/userservices/CareernovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careernova.userservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareernovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
